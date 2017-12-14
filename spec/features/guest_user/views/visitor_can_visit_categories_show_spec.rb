f94eee5d0a26e9ea7966d38aca7880711c3718e8
Marshall Houston <marshall.houston@gmail.com>
2017-12-14 09:57:45 -0700

    update test to visit correct route for category with items, add sad path test

--- spec/features/guest_user/views/visitor_can_visit_categories_show_spec.rb
+++ spec/features/guest_user/views/visitor_can_visit_categories_show_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 
-describe "As a visitor can visit category show page" do
-  it "can visit category show page" do
-    magic = create(:category, title: "Magic")
-    sci_fi = create(:category, title: "Sci-Fi")
-    create(:item, title: "Dove", category: magic)
-    create(:item, title: "Light Saber", category: sci_fi)
+describe "As a visitor I can visit category show page" do
+  context "with a valid URI /categories/category_name" do
+    it "and see a list of all active items for the category" do
+      magic = create(:category, title: "Magic")
+      sci_fi = create(:category, title: "Sci-Fi")
+      create(:item, title: "Dove", category: magic)
+      create(:item, title: "Rabbit", category: magic)
+      create(:item, title: "Light Saber", category: sci_fi)
 
-    visit '/magic'
+      visit '/categories/magic'
 
-    within(".items") do
-      expect(page).to have_content("Dove")
-      expect(page).to_not have_content("Light Saber")
+      within(".items") do
+        expect(page).to have_content("Dove")
+        expect(page).to have_content("Rabbit")
+        expect(page).to_not have_content("Light Saber")
+      end
+    end
+  end
+
+  context "with invalid URI /category_name" do
+    it "and I see a 404 error" do
+      create(:category, title: "Magic")
+
+      expect { visit '/magic' }.to raise_error(ActionController::RoutingError)
     end
   end
 end
