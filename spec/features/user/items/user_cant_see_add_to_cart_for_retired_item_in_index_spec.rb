0a2cba1445187a38d21811c1126a4bb14f4b0ee3
Adrian Lara <adrianblara@gmail.com>
2017-12-13 17:18:47 -0700

    remove unused string in 'user can't add retired item test'

--- spec/features/user/items/user_cant_see_add_to_cart_for_retired_item_in_index_spec.rb
+++ spec/features/user/items/user_cant_see_add_to_cart_for_retired_item_in_index_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'As a visitor' do
 	describe 'when visiting an items show page' do
 		it 'a user cannot see add to cart for a retired item' do
 			category = create(:category)
-			one_url = "http://pandathings.com/wp-content/uploads/2016/10/onesie-6-300x300.png"
 			item_one = create(:item, category: category, condition: 'retired')
 
 			visit items_path
