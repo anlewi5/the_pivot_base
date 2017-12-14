a4d668b2bd9b66a2fd48b3429f799c6175aa565e
Marshall Houston <marshall.houston@gmail.com>
2017-12-13 18:56:43 -0700

    add test:order_item to calculate total price before_save; add method on model

--- spec/models/order_items_spec.rb
+++ spec/models/order_items_spec.rb
@@ -1,49 +1,58 @@
 require 'rails_helper'
 
-describe "order item methods" do
-  it "returns the quantity for an item associated with an order" do
-    item_1 = create(:item)
-    item_2 = create(:item)
-    order_1 = create(:order, items: [item_1, item_2])
-    order_2 = create(:order, items: [item_1])
-    expected_result = {item_1.id => 2, item_2.id => 1}
-    quantity_hash = OrderItem.sum_quantity.count
-    expect(quantity_hash).to eq(expected_result)
+describe OrderItem do
+  describe "attributes" do
+    it "calculates the total_price when given a quantity and unit_price" do
+      order_item = create(:order_item, quantity: 1, unit_price: 0.25)
+      expect(order_item.total_price).to eq 0.25
+    end
   end
 
-  it "can return top three items" do
-    item_1 = create(:item)
-    item_2 = create(:item)
-    item_3 = create(:item)
-    item_4 = create(:item)
-    order_1 = create(:order)
-    order_2 = create(:order)
-
-    2.times do
-      order_1.items << item_1
-      order_2.items << item_2
-      order_2.items << item_4
+  describe "order item methods" do
+    it "returns the quantity for an item associated with an order" do
+      item_1 = create(:item)
+      item_2 = create(:item)
+      order_1 = create(:order, items: [item_1, item_2])
+      order_2 = create(:order, items: [item_1])
+      expected_result = {item_1.id => 2, item_2.id => 1}
+      quantity_hash = OrderItem.sum_quantity.count
+      expect(quantity_hash).to eq(expected_result)
     end
 
-    order_2.items << item_3
+    it "can return top three items" do
+      item_1 = create(:item)
+      item_2 = create(:item)
+      item_3 = create(:item)
+      item_4 = create(:item)
+      order_1 = create(:order)
+      order_2 = create(:order)
 
-    top_items = OrderItem.top_three_items
-    expect(top_items.count).to eq(3)
-    expect(top_items).to include(item_1)
-    expect(top_items).to include(item_2)
-    expect(top_items).to include(item_4)
-    expect(top_items).to_not include(item_3)
-  end
+      2.times do
+        order_1.items << item_1
+        order_2.items << item_2
+        order_2.items << item_4
+      end
 
-  it "can average quantity" do
-    item_1 = create(:item)
-    item_2 = create(:item)
-    order_1 = create(:order)
-    order_2 = create(:order)
+      order_2.items << item_3
 
-    create(:order_item, item: item_1, quantity: 1, order: order_1)
-    create(:order_item, item: item_2, quantity: 3, order: order_2)
+      top_items = OrderItem.top_three_items
+      expect(top_items.count).to eq(3)
+      expect(top_items).to include(item_1)
+      expect(top_items).to include(item_2)
+      expect(top_items).to include(item_4)
+      expect(top_items).to_not include(item_3)
+    end
 
-    expect(OrderItem.average_quantity).to eq(2)
+    it "can average quantity" do
+      item_1 = create(:item)
+      item_2 = create(:item)
+      order_1 = create(:order)
+      order_2 = create(:order)
+
+      create(:order_item, item: item_1, quantity: 1, order: order_1)
+      create(:order_item, item: item_2, quantity: 3, order: order_2)
+
+      expect(OrderItem.average_quantity).to eq(2)
+    end
   end
 end
