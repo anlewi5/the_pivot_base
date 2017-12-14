06bda57711c7319b99ecfacee3a5ef0a4bbf584d
Marshall Houston <marshall.houston@gmail.com>
2017-12-14 09:56:52 -0700

    change test route expectation /categories/category_name

--- spec/features/user/views/user_can_visit_category_show_spec.rb
+++ spec/features/user/views/user_can_visit_category_show_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
-RSpec.describe "As a visitor can visit category show page" do
-    it " can visit category show page" do
-      category = create(:category)
-      item = create(:item, category: category)
 
-      visit category_path(category)
-    end
+RSpec.describe "As a visitor can visit category show page" do
+  it " can visit category show page" do
+    category = create(:category)
+
+    visit category_path(category)
+
+    expect(current_path).to eq("/categories/#{category.slug}")
+  end
 end
