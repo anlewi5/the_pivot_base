dfa128a5a234b0040868aa0447f67d63a0b8c1f0
Marshall Houston <marshall.houston@gmail.com>
2017-12-13 17:12:52 -0700

    add migration for total_price to order_items, update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213211302) do
+ActiveRecord::Schema.define(version: 20171214001153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20171213211302) do
     t.datetime "updated_at", null: false
     t.integer "quantity"
     t.float "unit_price"
+    t.float "total_price"
     t.index ["item_id"], name: "index_order_items_on_item_id"
     t.index ["order_id"], name: "index_order_items_on_order_id"
   end
