f0d7bdd36e0689abd4c526421d46cbadfe2cb2e4
Adrian Lara <adrianblara@gmail.com>
2017-12-14 13:52:06 -0700

    adjust test to reflect base functionality/requirements of current ticket

--- spec/features/guest_user/stores/visitor_visits_different_stores_spec.rb
+++ spec/features/guest_user/stores/visitor_visits_different_stores_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 describe "As a guest, when I visit '/store-name-1'" do
-  it "I see the store name, a list of all items associated to that store, and each items name, stock image, price, and 'Add to Cart' button." do
+  it "I see the store name, a list of all items associated to that store, and each items name, price, and 'Add to Cart' button." do
     stores = create_list(:store, 2)
     items_store_1 = create_list(:item, 2, store: stores.first)
     items_store_2 = create_list(:item, 2, store: stores.last)
 
-    visit store_path(stores.first)
+    visit store_path(store: stores.first.url)
 
-    expect(current_path).to eq("/#{stores.first.name}")
+    expect(current_path).to eq("/#{stores.first.url}")
+
+    expect(page).to have_content(stores.first.name)
 
     items_store_1.each do |item|
-      expect(page).to have_content(item.name)
-      expect(page).to have_css("imgasdfasdfsadf")
+      expect(page).to have_content(item.title)
       expect(page).to have_content(item.price)
-      expect(page).to have_content("check for button ")
+      expect(page).to have_link("Add to cart")
     end
   end
 end
