8b066c2bce8a3fffcfd17680768e177343012484
Adrian Lara <adrianblara@gmail.com>
2017-12-14 16:30:47 -0700

    admin item creation test adjusted to now associate new item to store and category. test was not executing as expected because category wasn't being associated before this

--- spec/features/admin/items/admin_can_create_items_spec.rb
+++ spec/features/admin/items/admin_can_create_items_spec.rb
@@ -4,7 +4,8 @@ RSpec.feature "Admin item creation" do
   context "As an authenticated admin" do
     it "I can create an item" do
       admin = build(:admin)
-      create(:item)
+      store = create(:store)
+      category = create(:category)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       visit admin_items_path
@@ -12,6 +13,8 @@ RSpec.feature "Admin item creation" do
       fill_in "item[title]", with: "Onesie"
       fill_in "item[description]", with: "This Onesie is awesome!"
       fill_in "item[price]", with: "59.99"
+      select store.name, from: "item[store_id]"
+      select category.title, from: "item[category_id]"
       page.attach_file("item[image]", testing_image)
       click_on "Create Item"
 
@@ -22,6 +25,7 @@ RSpec.feature "Admin item creation" do
 
     it "I can create an item without an image and it defaults" do
       admin = build(:admin)
+      store = create(:store)
       category = create(:category)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
@@ -31,6 +35,8 @@ RSpec.feature "Admin item creation" do
       fill_in "item[title]", with: "Onesie"
       fill_in "item[description]", with: "This Onesie is awesome!"
       fill_in "item[price]", with: "59.99"
+      select store.name, from: "item[store_id]"
+      select category.title, from: "item[category_id]"
       click_on "Create Item"
 
       expect(current_path).to eq(admin_items_path)
