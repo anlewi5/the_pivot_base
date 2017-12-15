21a32f4fe29cb9d70f6bd928885b0e30da236cb9
Adrian Lara <adrianblara@gmail.com>
2017-12-14 18:42:51 -0700

    admin items controller create and update methods refactored further since initial bug fix changed these lines of code anyway

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,8 +11,8 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
+    item = Item.new(item_params)
+    if item.save
       redirect_to admin_items_path
     else
       redirect_to new_admin_item_path
@@ -20,12 +20,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
+    item = Item.find(params[:id])
+    if item.update(item_params)
       redirect_to admin_items_path
     else
-      redirect_to edit_admin_item_path(@item)
+      redirect_to edit_admin_item_path(item)
     end
   end
 
