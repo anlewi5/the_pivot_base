f7f2263b39cac4fb2696a2c81cf5bbf672460172
izaxveta <izavetha@gmail.com>
2017-12-14 21:01:05 -0700

    Added '.' punctuation to flash messages for consistency across all flash messages for when all user types log in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,14 +22,14 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       login_successful
     else
-      flash[:failure] = "That login was unsuccessful"
+      flash[:failure] = "That login was unsuccessful."
       redirect_to login_path
     end
   end
 
   def login_successful
     session[:user_id] = @user.id
-    flash[:notice] = "Logged in as #{@user.first_name} #{@user.last_name}"
+    flash[:notice] = "Logged in as #{@user.first_name} #{@user.last_name}."
     if @user.role == "admin"
       redirect_to admin_dashboard_index_path
     elsif @user.role == "default"
