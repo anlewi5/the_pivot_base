e83616b4bf6775f9b03059d8824e17df7dbfe1f5
Adrian Lara <adrianblara@gmail.com>
2017-12-14 18:53:52 -0700

    guest visits store show test is more robust

--- spec/features/guest_user/stores/visitor_visits_different_stores_spec.rb
+++ spec/features/guest_user/stores/visitor_visits_different_stores_spec.rb
@@ -4,7 +4,7 @@ describe "As a guest, when I visit '/store-name-1'" do
   it "I see the store name, a list of all items associated to that store, and each items name, price, and 'Add to Cart' button." do
     stores = create_list(:store, 2)
     items_store_1 = create_list(:item, 2, store: stores.first)
-    items_store_2 = create_list(:item, 2, store: stores.last)
+    items_store_2 = create_list(:item, 2, store: stores.last, price: 11.00)
 
     visit store_path(store: stores.first.url)
 
@@ -16,6 +16,11 @@ describe "As a guest, when I visit '/store-name-1'" do
       expect(page).to have_content(item.title)
       expect(page).to have_content(item.price)
       expect(page).to have_link("Add to cart")
+    end
+
+    items_store_2.each do |item|
+      expect(page).not_to have_content(item.title)
+      expect(page).not_to have_content(item.price)
     end
   end
 end
