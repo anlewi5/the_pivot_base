69a5d91ce2ff470efdc4507e3e62447db319feb3
izaxveta <izavetha@gmail.com>
2017-12-15 20:27:30 -0700

    Store has_many users/user_roles, .create_with_user, #associate_user, #generate_url callback

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,24 @@
 class Store < ApplicationRecord
-
+  has_many :user_roles
+  has_many :users, through: :user_roles
   has_many :items
+
+  before_validation :generate_url
 
   enum status: ["pending", "active", "suspended"]
 
+  def self.create_with_user(store_info, user)
+    create(store_info).associate_user(user)
+  end
+
+  def associate_user(user)
+    role = Role.find_by(name: "Registered User")
+    user_roles.create(user:user, role: role)
+  end
+
+  private
+
+    def generate_url
+      self.url = name.parameterize
+    end
 end
