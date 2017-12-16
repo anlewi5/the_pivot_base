dfc2f1a3bdb5f4dc99c9c9d4d3f0d89c29df473e
izaxveta <izavetha@gmail.com>
2017-12-15 20:23:48 -0700

    Connected relations between user and user_role pertaining to user having many stores

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :user_roles
   has_many :roles, through: :user_roles
+  has_many :stores, through: :user_roles
   has_many :orders
 
   validates :first_name, :last_name, :password, presence: true
