5ccb1fad206aca290845f467dea953ecf488cd51
Marshall Houston <marshall.houston@gmail.com>
2017-12-16 15:29:17 -0700

    update store to be active

--- spec/features/guest_user/stores/visitor_visits_different_stores_spec.rb
+++ spec/features/guest_user/stores/visitor_visits_different_stores_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "As a guest, when I visit '/store-name-1'" do
   it "I see the store name, a list of all items associated to that store, and each items name, price, and 'Add to Cart' button." do
-    stores = create_list(:store, 2)
+    stores = create_list(:store, 2, status: "active")
     items_store_1 = create_list(:item, 2, store: stores.first)
     items_store_2 = create_list(:item, 2, store: stores.last, price: 11.00)
 
