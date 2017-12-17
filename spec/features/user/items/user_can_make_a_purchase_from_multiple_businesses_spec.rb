ad1f3a731135879f64e4b419e7476daf6b151c50
Marshall Houston <marshall.houston@gmail.com>
2017-12-17 16:05:58 -0700

    add test for user making purchases from multiple businesses

---
+++ spec/features/user/items/user_can_make_a_purchase_from_multiple_businesses_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "As a logged in user with items from different stores in my cart" do
+  before :each do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    stores = create_list(:store, 2, status: "active")
+    create(:item, store: stores.first)
+    create(:item, store: stores.last)
+
+    visit '/'
+    click_on "Stores"
+    click_on stores.first.name
+    click_on "Add to cart"
+    click_on "Stores"
+    click_on stores.second.name
+    click_on "Add to cart"
+    click_on "Cart"
+  end
+
+  scenario "I can make a purchase" do
+    click_on "Checkout"
+    expect(current_path).to eq(orders_path)
+    expect(page).to have_content("Order was successfully placed")
+  end
+end
