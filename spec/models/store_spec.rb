845c066af193e84dcd5e5dd58ae8f26f01f53654
Marshall Houston <marshall.houston@gmail.com>
2017-12-16 22:53:01 -0700

    remove test because class method was removed

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,24 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Store do
-  describe ".class_methods" do
-    describe ".all_active_stores" do
-      it "returns only active stores" do
-        store_1 = create(:store, status: "active")
-        store_2 = create(:store, status: "pending")
-        store_3 = create(:store, status: "suspended")
-
-        active_stores = Store.all_active_stores
-
-        expect(active_stores).to include(store_1)
-        expect(active_stores).to_not include(store_2)
-        expect(active_stores).to_not include(store_3)
-      end
-    end
-  end
   describe "#instance_methods" do
     let(:store) { create(:store) }
-    
+
     describe "#active_items" do
       it "returns only active items" do
         item_1 = create(:item, store: store, condition: "active")
