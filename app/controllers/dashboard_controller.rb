32e3415ec7a4fd949c6ad4e2984cc5cd385aaeee
izaxveta <izavetha@gmail.com>
2017-12-17 21:00:56 -0700

    Expanded index action to ensure flexibility of those attempting to access '/dashboard'

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,9 @@ class DashboardController < ApplicationController
   def index
     if current_user.nil?
       redirect_to login_path
-    else
+    elsif current_user.current_admin?
+      redirect_to admin_dashboard_index_path
+    elsif current_user.registered_user?
       @user = User.find(current_user.id)
     end
   end
