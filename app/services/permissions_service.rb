341d7dd13c34df2fb30ec60b2d9096f69683a00f
izaxveta <izavetha@gmail.com>
2017-12-18 09:30:23 -0700

    refactored PermissionsService

--- app/services/permissions_service.rb
+++ app/services/permissions_service.rb
@@ -23,45 +23,39 @@ class PermissionsService
     attr_reader :user, :controller, :action
 
     def platform_admin_permissions
-      return true if controller == 'main' && action == 'index'
-      return true if controller == 'sessions' && action.in?(%w(new create destroy))
-      return true if controller == 'admin/dashboard' && action == 'index'
       return true if controller == 'users' && action.in?(%w(edit update))
       return true if controller == 'admin/stores' && action.in?(%w(index update))
+      store_admin_permissions
     end
 
     def store_admin_permissions
-      return true if controller == 'sessions' && action.in?(%w(new create destroy))
-      return true if controller == 'admin/dashboard' && action == 'index'
-      return true if controller == 'orders' && action.in?(%w(index show new))
-      return true if controller == 'admin/items' && action.in?(%w(index show new create edit update))
+      return true if controller == 'admin/items' && action.in?(%w(new create))
+      store_manager_permissions
     end
 
     def store_manager_permissions
       return true if controller == 'admin/dashboard' && action == 'index'
       return true if controller == 'admin/items' && action.in?(%w(index show edit update))
-      return true if controller == 'orders' && action.in?(%w(index show update new))
+      return true if controller == 'orders' && action.in?(%w(update new))
+      registered_user_permissions
     end
 
     def registered_user_permissions
-      return true if controller == 'main' && action == 'index'
-      return true if controller == 'sessions' && action.in?(%w(new create destroy))
-      return true if controller == 'dashboard' && action == 'index'
-      return true if controller == 'orders' && action.in?(%w(index show new))
-      return true if controller == 'stores' && action.in?(%w(show new create))
       return true if controller == 'user/stores' && action.in?(%w(index))
-      return true if controller == 'carts' && action.in?(%w(index create))
+      return true if controller == 'stores' && action.in?(%w(index show new create))
+      return true if controller == 'orders' && action.in?(%w(index show))
+      guest_user_permissions
     end
 
     def guest_user_permissions
       return true if controller == 'main' && action == 'index'
-      return true if controller == 'dashboard' && action == 'index'
       return true if controller == 'sessions' && action.in?(%w(new create destroy))
-      return true if controller == 'items' && action.in?(%w(index show))
-      return true if controller == 'orders' && action.in?(%w(new))
-      return true if controller == 'carts' && action.in?(%w(index create destroy update))
-      return true if controller == 'categories' && action.in?(%w(index show))
+      return true if controller == 'dashboard' && action == 'index'
       return true if controller == 'stores' && action.in?(%w(index show))
+      return true if controller == 'categories' && action.in?(%w(index show))
+      return true if controller == 'items' && action.in?(%w(index show))
+      return true if controller == 'carts' && action.in?(%w(index create destroy update))
+      return true if controller == 'orders' && action.in?(%w(new))
       return true if controller == 'users' && action.in?(%w(new create))
     end
 end
