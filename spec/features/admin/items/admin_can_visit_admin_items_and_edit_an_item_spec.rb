5ebc37f7596371e45e8f60325157b00dd4f8877f
Marshall Houston <marshall.houston@gmail.com>
2017-12-18 16:00:36 -0700

    update other tests to account for scoping to a specific store based on user

--- spec/features/admin/items/admin_can_visit_admin_items_and_edit_an_item_spec.rb
+++ spec/features/admin/items/admin_can_visit_admin_items_and_edit_an_item_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-
 RSpec.describe 'an admin can visit admin dashboard' do
   describe 'and see a link for all items' do
     it 'when clicked that link should be the admin item index with admin functionality' do
-      create(:item)
+      store = create(:store)
+      create(:item, store: store)
       admin = create(:admin)
       role = create(:store_admin)
-      admin.roles << role
+      create(:user_role, user: admin, role: role, store: store)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       visit admin_items_path
