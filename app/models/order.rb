de75a141f4f52f8ef11b95b45d8708c9e2206437
Marshall Houston <marshall.houston@gmail.com>
2017-12-18 20:53:48 -0700

    add filtering of orders by admin. create methods on Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
   validates :status, presence: true
   has_many :order_items
   has_many :items, through: :order_items
+  belongs_to :store
 
   enum status: ["ordered", "paid", "cancelled", "completed"]
 
@@ -18,8 +19,12 @@ class Order < ApplicationRecord
     group(:status).count
   end
 
-  def self.filter_by_status(status)
-    where(status: status)
+  def self.filter_by_status(status, user)
+    if user.platform_admin?
+      where(status: status)
+    else
+      where(store: user.stores).where(status: status)
+    end
   end
 
   def self.count_of_completed_orders
@@ -29,4 +34,13 @@ class Order < ApplicationRecord
   def self.shop_total_gross
 		where(status: :completed).joins(:items).sum(:price)
   end
+
+  def self.all_for_admin(user)
+    if user.platform_admin?
+      Order.all
+    else
+      where(store: user.stores)
+    end
+  end
+
 end
