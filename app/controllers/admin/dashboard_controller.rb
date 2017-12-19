16065261f709fbacc178a6eb44ba82ae8008392d
Marshall Houston <marshall.houston@gmail.com>
2017-12-18 20:53:48 -0700

    add filtering of orders by admin. create methods on Order

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,9 +3,9 @@ class Admin::DashboardController < ApplicationController
 
   def index
     if params[:status] == "ordered" || params[:status] == "paid" || params[:status] == "cancelled" || params[:status] == "completed"
-      @orders = Order.filter_by_status(params[:status])
+      @orders = Order.filter_by_status(params[:status], current_user)
     else
-      @orders = Order.all
+      @orders = Order.all_for_admin(current_user)
     end
     flash[:notice] = "You're logged in as #{current_user.highest_role}."
   end
