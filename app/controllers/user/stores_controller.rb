049c785c9a2766d66f51d8ebeb4dff24db463c38
izaxveta <izavetha@gmail.com>
2017-12-18 18:30:43 -0700

    Created edit/update actions and defined strong params for User/StoresController

--- app/controllers/user/stores_controller.rb
+++ app/controllers/user/stores_controller.rb
@@ -2,4 +2,25 @@ class User::StoresController < ApplicationController
   def index
     @stores = current_user.stores
   end
+
+  def edit
+    @store = Store.find(params[:id])
+  end
+
+  def update
+    store = Store.find(params[:format])
+    if store.update(store_params)
+      flash[:success] = "Store name successfully updated!"
+      redirect_to user_stores_path
+    else
+      flash[:success] = "Oops! Something went wrong."
+      render :edit
+    end
+  end
+
+  private
+
+    def store_params
+      params.require(:store).permit(:name)
+    end
 end
