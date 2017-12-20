9f369ce7298439414b1df88cb8b262a7c0f0d0e8
Adrian Lara <adrianblara@gmail.com>
2017-12-19 20:01:38 -0700

    Increase security for a store admin promoting or firing employees

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -5,9 +5,16 @@ class Admin::EmployeesController < ApplicationController
   end
 
   def update
-    UserRole.admin_update(params[:id], params[:store_id], params[:fire])
+    UserRole.admin_update(user_role_params)
 
     redirect_to admin_employees_path
   end
+
+  private
+
+    def user_role_params
+      not_found unless User.find(current_user.id).stores.find(params[:store_id])
+      params.permit(:id, :store_id, :fire)
+    end
 
 end
