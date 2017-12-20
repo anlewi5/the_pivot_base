332b401910c9d0dbced86522a4aa4fcd67f1e82c
Marshall Houston <marshall.houston@gmail.com>
2017-12-20 12:56:07 -0700

    WIP search api. add serializer gem

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SearchController < Api::ApplicationController
-
+  
   def index
-    render json: search_api_service.search
+    render json: search_api_service.search, to: SearchSerializer
   end
 
   private
--- app/services/search_api_service.rb
+++ app/services/search_api_service.rb
@@ -1,15 +1,17 @@
 class SearchApiService
-
+  
   def initialize(attrs = {})
     @type = attrs[:type]
     @search_word = attrs[:q]
   end
 
   def search
-    
+    if type == 'items' && search_word
+      Item.where("title like ? OR description like ?", "%#{search_word}%", "%#{search_word}%")
+    end
   end
 
   private
-    attr_reader :type, :keyword
+    attr_reader :type, :search_word
 
 end
