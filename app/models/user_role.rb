6b11cd6f3943775d959d95d0ac45c9aa0d9f7383
Adrian Lara <adrianblara@gmail.com>
2017-12-19 17:50:55 -0700

    Method written to either fire or promote a store's user

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -2,4 +2,19 @@ class UserRole < ApplicationRecord
   belongs_to :user
   belongs_to :role
   belongs_to :store, optional: true
+
+  def self.admin_update(user_id, store_id, fire = nil)
+    user_role = find_by(user_id: user_id, store_id: store_id)
+    return user_role.destroy if fire
+
+    promote_user(user_role)
+  end
+
+  private
+
+    def self.promote_user(user_role)
+      role = Role.find_by(name: "Store Admin")
+
+      user_role.update(role: role)
+    end
 end
