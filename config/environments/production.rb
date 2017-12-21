8624ef32a4d6b8797995d2a33ceca59317d7533d
Adrian Lara <adrianblara@gmail.com>
2017-12-21 12:04:16 -0700

    remove previous change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,6 @@ Rails.application.configure do
     storage: :s3,
     preserve_files: true,
     s3_region: ENV['AWS_REGION'],
-    url: :s3_domain_url,
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
