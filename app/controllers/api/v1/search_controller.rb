22e56114c00a34e0c470324fd11117898d68137d
Adrian Lara <30608004+adrian-lara@users.noreply.github.com>
2017-12-21 14:44:05 -0700

    Revert "Revert "WIP search api""
    This reverts commit aac8b7bcc59f84944134d0858477462ff4544986.

---
+++ app/controllers/api/v1/search_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::SearchController < Api::ApplicationController
+
+  def index
+    render json: search_api_service.search
+  end
+
+  private
+    def search_params
+      params.permit(:type, :q)
+    end
+
+    def search_api_service
+      SearchApiService.new(search_params)
+    end
+end
