03a02f6d7320e0cbf572a2770d787477b851ed81
Adrian Lara <30608004+adrian-lara@users.noreply.github.com>
2017-12-21 14:43:26 -0700

    Revert "remove comments in code"
    This reverts commit 51fb98099e08be1c03a9a4761402c6f9f9b82e4f.

--- spec/requests/api/v1/search_spec.rb
+++ spec/requests/api/v1/search_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
+# GET "/api/v1/search?type=items&q=diapers&api_key=YOUR_REAL_KEY"
+#
+# The above query should return all items with a title or description containing the word "diapers"
+# At some point we might want to allow searching other tables and but let's stick to only items for now.
+
+# Return data should be JSON and like this:
+# {
+#   "type": "items",
+#   "q": "diapers",
+#   "results": [
+#     {
+#       "id": 1,
+#       "title": "Nature's Diapers",
+#       "description": "No chemicals or dyes. Also, they don't really work.",
+#       "price": "$30.00"
+#     },
+#     {
+#       "id": 103,
+#       "title": "Dopo Tesigns Shoulder Bag",
+#       "description": "Versitile bag great for commuting. Even makes as an excellent diaper bag. #dadcore #momcore",
+#       "price": "$125.00"
+#     }
+#   ]
+# }
 
 describe "Search Items API" do
   describe "get /api/v1/search?type=items&q='keyword'" do
