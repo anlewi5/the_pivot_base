66a15a17761b1f2b8fd0eaef246536e790a62bbf
Adrian Lara <adrianblara@gmail.com>
2017-12-21 12:16:26 -0700

    moved s3 region into credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,9 +92,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     preserve_files: true,
-    s3_region: ENV['AWS_REGION'],
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
+      s3_region: ENV['AWS_REGION'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
