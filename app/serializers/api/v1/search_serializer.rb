1a14b8ba17897e8317c13f594eaa8795f7a2523f
Adrian Lara <30608004+adrian-lara@users.noreply.github.com>
2017-12-21 14:44:05 -0700

    Revert "WIP search-api"
    This reverts commit f1edd5c090c444d78bf5f5a8e9abc1d8fdf1e3cd.

--- app/serializers/api/v1/search_serializer.rb
+++ app/serializers/api/v1/search_serializer.rb
@@ -1,6 +1,7 @@
 class Api::V1::SearchSerializer < ActiveModel::Serializer
   extend ItemSerializer
   extend StoreSerializer
-  extend UserSerializer
+  extend OrderSerializer
   extend CategorySerializer
+
 end
