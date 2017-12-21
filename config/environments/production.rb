2d621c15cc1ef5b3d1d5c8865501efff0e76116c
Adrian Lara <adrianblara@gmail.com>
2017-12-21 11:31:05 -0700

    remove aws storage name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,6 @@ Rails.application.configure do
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      s3_host_name: ENV["AWS_S3_HOST_NAME"],
       s3_region: ENV['AWS_REGION']
     }
   }
