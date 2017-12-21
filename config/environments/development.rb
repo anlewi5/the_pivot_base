6ce782377fcc541d7c2dd219a82ebf4cc6bbfb8a
Adrian Lara <adrianblara@gmail.com>
2017-12-21 11:19:32 -0700

    aws images

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.paperclip_defaults = {
+    path: ":rails_root/public/images/:id/:style/:filename",
+    url: "/images/:id/:style/:filename"
+  }
 end
